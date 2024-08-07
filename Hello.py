dc34ea59dfde3029ccdd1af52705154f6cccad28
Ana Olaso Rojo <aolaso-r@c4r7s4.42urduliz.com>
2024-08-07 14:37:53 +0200

    primer intento

---
+++ Hello.py
@@ -0,0 +1,9 @@
+ft_list = ["Hello, "World"]
+ft_tuple = ("Hello", "Spain")
+ft_set = {"Hello", "Urduliz"}
+ft_dict = {"Hello" : "Urduliz42""}
+
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
+print(ft_dict)
---
+++ version.py
@@ -0,0 +1,2 @@
+import sys
+print ("estoy usando Python" + sys.version + "\n")
