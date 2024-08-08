a192a978a991fadd6729a35962c2b435a8d9f7ae
Ana <aolasor@gmail.com>
2024-08-08 15:35:43 +0200

    incluyo ex01

--- format_ft_type.py
+++ format_ft_type.py
@@ -1,1 +1,9 @@
+#the current unix time
 
+timestamp= time.time()
+
+#Format the current data in the desired format
+current_time=time.strftime(%b %d %Y, time.localtime(timestamp))
+#print the results
+print(f"Seconds since January 1, 1970: {timestamp:,.4f} or", f"{timestamp:.2e} in scientific notation")
+print(current_time)
